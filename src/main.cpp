8a0dde929b178c65414f769a13ae3f548cefb5bc
royyandzakiy <royyan.abdul@gmail.com>
2023-07-16 19:46:12 +0100

    changed nature of loop to alter between a sending mode and receiving mode. in sending mode, after it successfully sends, it expects a message to be received from the target node
    in receive mode, it just receives

--- src/main.cpp
+++ src/main.cpp
@@ -6,6 +6,7 @@
 #include <RHMesh.h>
 #include <RH_RF95.h>
 #include <SPI.h>
+
 #include <cstring>
 
 // pinout
@@ -16,33 +17,26 @@
 // MOSI   -------------- GPIO23
 // MISO   -------------- GPIO19
 // DIO0   -------------- GPIO2
-// 
+//
 // 3.3V   -------------- 3.3V
 // GND    -------------- GND
 
 #define RFM95_CS 5
 #define RFM95_RST 14
 #define RFM95_INT 2
-
-// Change to 434.0 or other frequency, must match RX's freq!
 #define RF95_FREQ 915.0
-
-// Mesh has much greater memory requirements, and you may need to limit the
-// max message length (characters) to prevent wierd crashes
 #define RH_MESH_MAX_MESSAGE_LEN 50
 
-// This example of 4 node topology, in which the FINAL_ADDRESS node is
-// expected to be the last node, simulating a common network in which this
-// last node would be a border node connected to the internet, collecting
-// messages from other nodes during it's lifetime. whilst node 1-3 would
-// commonly send sensor data, and one could be an intermediary node for
-// another
+// Topology
 #define NODE1_ADDRESS 1
 #define NODE2_ADDRESS 2
 #define NODE3_ADDRESS 3
 #define FINAL_ADDRESS 4
 
-// change this!
+#define SENDING_MODE 0
+#define RECEIVING_MODE 1
+
+// CHANGE THIS!!!
 #define SELF_ADDRESS NODE3_ADDRESS
 #define TARGET_ADDRESS FINAL_ADDRESS
 
@@ -60,32 +54,53 @@ void setup() {
   rhSetup();
 }
 
-void loop() {
-  Serial.println("Sending to " + TARGET_ADDRESS);
+long _lastSend = 0, _sendDuration = 0;
+uint8_t mode = SENDING_MODE;
 
-  // Send a message to another rhmesh node
-  // A route to the destination will be automatically discovered.
-  if (RHMeshManager.sendtoWait(reinterpret_cast<uint8_t *>(&msgSend[0]),
-                               msgSend.size(),
-                               TARGET_ADDRESS) == RH_ROUTER_ERROR_NONE) {
-    // It has been reliably delivered to the next node.
-    // Now wait for a reply from the target node
-    uint8_t _msgFrom;
-    uint8_t _msgRcvBuf[RH_MESH_MAX_MESSAGE_LEN];
-    if (RHMeshManager.recvfromAckTimeout(_msgRcvBuf, (uint8_t *) sizeof(_msgRcvBuf), 3000, &_msgFrom)) {
-      msgRcv = std::string("got reply from : " + std::to_string(_msgFrom) + ":" + reinterpret_cast<char*>(_msgRcvBuf));
+void loop() {
+  uint8_t _msgFrom;
+  uint8_t _msgRcvBuf[RH_MESH_MAX_MESSAGE_LEN];
+
+  if (millis() - _lastSend > _sendDuration) {
+    mode = SENDING_MODE;
+  }
+
+  if (mode == SENDING_MODE) {
+    // Send a message to another rhmesh node
+    Serial.println("Sending to " + TARGET_ADDRESS);
+    if (RHMeshManager.sendtoWait(reinterpret_cast<uint8_t *>(&msgSend[0]),
+                                 msgSend.size(),
+                                 TARGET_ADDRESS) == RH_ROUTER_ERROR_NONE) {
+      // message successfully be sent to the target node, or next neighboring
+      // expecting to recieve a simple reply from the target node
+      if (RHMeshManager.recvfromAckTimeout(
+              _msgRcvBuf, (uint8_t *)sizeof(_msgRcvBuf), 3000, &_msgFrom)) {
+        msgRcv = std::string("got reply from : " + std::to_string(_msgFrom) +
+                             ":" + reinterpret_cast<char *>(_msgRcvBuf));
+      } else {
+        Serial.println("No reply, is the target node running?");
+      }
     } else {
-      Serial.println("No reply, are the other nodes running?");
+      Serial.println("sendtoWait failed. No response from intermediary node, are they running?");
     }
-  } else
-    Serial.println(
-        "sendtoWait failed. Are the other nodes running?");
+    _lastSend = millis();
+    mode = RECEIVING_MODE;
+  }
+
+  if (mode == RECEIVING_MODE) {
+    // while at it, wait for a message from other nodes
+    if (RHMeshManager.recvfromAckTimeout(
+            _msgRcvBuf, (uint8_t *)sizeof(_msgRcvBuf), 3000, &_msgFrom)) {
+      msgRcv = std::string("got msg from : " + std::to_string(_msgFrom) + ":" +
+                           reinterpret_cast<char *>(_msgRcvBuf));
+    } else {
+      Serial.println("No messages coming in...");
+    }
+  }
 }
 
 void rhSetup() {
   if (!RHMeshManager.init()) Serial.println("init failed");
-  // Defaults after init are 434.0MHz, 0.05MHz AFC pull-in, modulation
-  // FSK_Rb2_4Fd36. change to this
   RHDriver.setTxPower(23, false);
   RHDriver.setFrequency(RF95_FREQ);
   RHDriver.setCADTimeout(500);
